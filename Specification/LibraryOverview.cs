4fc89efb39029a3d236eb0de1743a7cafbdd97d0
VersusImpossible <leonardo.ero13@gmail.com>
2017-06-02 18:54:34 +0700

    Create LibraryOverview.cs
    This shows list of books

--- Specification/LibraryOverview.cs
+++ Specification/LibraryOverview.cs
@@ -1,28 +1,34 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using System;
 using TestStack.BDDfy;
 
 namespace ReadOne.Specification
 {
     [Story(AsA = "As a reader",
-           IWant = "I want to list all books",
+           IWant = "I want to view all books",
            SoThat = "So that I can overview all library content")]
     public class LibraryOverview
     {
         [Test]
-        public void BobIsGreeted()
+        public void ListOfBooks()
         {
-            this.When(x => x.BobSaysAnything())
-                .Then(x => x.HeSeesHi())
+            this.When(x => x.ShowMeBooks())
+                  .And(x => x.ListOfBooksIsNotEmpty())
+                .Then(x => x.YouSeesListOfBooks())
                 .BDDfy();
         }
 
-        private void HeSeesHi()
+        private void YouSeesListOfBooks()
         {
             throw new NotImplementedException();
         }
 
-        private void BobSaysAnything()
+        private void ShowMeBooks()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void ListOfBooksIsNotEmpty()
         {
             throw new NotImplementedException();
         }
