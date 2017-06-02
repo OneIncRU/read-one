27611169994b2d03317d3ec02707bf9b813ab3e5
Eugene Griaznov <evgeniy.gryaznov@gmail.com>
2017-06-02 17:45:01 +0700

    The empty solution with specification example is added

---
+++ Specification/LibraryOverview.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using TestStack.BDDfy;
+
+namespace ReadOne.Specification
+{
+    [Story(AsA = "As a reader",
+           IWant = "I want to list all books",
+           SoThat = "So that I can overview all library content")]
+    public class LibraryOverview
+    {
+        [Test]
+        public void BobIsGreeted()
+        {
+            this.When(x => x.BobSaysAnything())
+                .Then(x => x.HeSeesHi())
+                .BDDfy();
+        }
+
+        private void HeSeesHi()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BobSaysAnything()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
