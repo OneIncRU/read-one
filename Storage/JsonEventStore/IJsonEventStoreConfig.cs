2b36c1bf3b58369b2bf48ef6d9fd12ff8c01ee43
Vadim Klyapov <vklyapov@oneincsystems.com>
2017-06-02 20:17:28 +0700

    Create EventStore as json files

---
+++ Storage/JsonEventStore/IJsonEventStoreConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Storage.JsonEventStore
+{
+    public interface IJsonEventStoreConfig
+    {
+        string EventStoreRootDir { get; }
+    }
+
+    public class JsonEventStoreConfig : IJsonEventStoreConfig
+    {
+        public string EventStoreRootDir { get; set; }
+    }
+}
