359c699f8c11ff1b523667147b1502612481c1ba
Eugene Griaznov <evgeniy.gryaznov@gmail.com>
2017-06-03 16:52:29 +0700

    Application is initialized with two books on start now

--- Application/ReadOne.cs
+++ Application/ReadOne.cs
@@ -1,7 +1,37 @@
-﻿namespace ReadOne.Application
+﻿using System;
+
+namespace ReadOne.Application
 {
     public class ReadOne
     {
+        public ReadOne()
+        {
+            _store = new InMemoryEventStore();
+            _dispatcher = new MessageDispatcher(_store);
+
+            _dispatcher.ScanInstance(new Book());
+
+            _library = new Library();
+            _dispatcher.ScanInstance(_library);
+            _commands = new Language();
+            _dispatcher.ScanInstance(_commands);
+        }
+
+        public void Init()
+        {
+            Do(new Add
+            {
+                Id = Guid.NewGuid(),
+                Name = "Implementing Domain-Driven Design"
+            });
+
+            Do(new Add
+            {
+                Id = Guid.NewGuid(),
+                Name = "Impact Mapping: Making a Big Impact with Software Products and Projects"
+            });
+        }
+
         public void Do(ICommand command)
         {
             _dispatcher.SendCommand(command);
@@ -17,18 +47,7 @@
             return _library.Books(tags);
         }
 
-        public ReadOne()
-        {
-            _dispatcher = new MessageDispatcher(new InMemoryEventStore());
-
-            _dispatcher.ScanInstance(new Book());
-
-            _library = new Library();
-            _dispatcher.ScanInstance(_library);
-            _commands = new Language();
-            _dispatcher.ScanInstance(_commands);
-        }
-
+        private readonly IEventStore _store;
         private readonly MessageDispatcher _dispatcher;
         private readonly Library _library;
         private readonly Language _commands;
