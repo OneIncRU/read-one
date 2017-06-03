97c1a1f365c453ecf87838794e63807ef96a70b9
Vadim Klyapov <vklyapov@oneincsystems.com>
2017-06-03 12:47:03 +0700

    style fix

--- Bot/Dialogs/RootDialog.cs
+++ Bot/Dialogs/RootDialog.cs
@@ -20,10 +20,10 @@ namespace Bot.Dialogs
             var activity = await result as Activity;
 
             // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            var length = (activity?.Text ?? string.Empty).Length;
 
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            await context.PostAsync($"You sent {activity?.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
         }
