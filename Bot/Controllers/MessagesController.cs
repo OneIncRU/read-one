44dc136f0df5e1d704dd6fd77a1a783cd4808194
Vadim Klyapov <vklyapov@oneincsystems.com>
2017-06-03 17:47:21 +0700

    base scenario

--- Bot/Controllers/MessagesController.cs
+++ Bot/Controllers/MessagesController.cs
@@ -1,5 +1,7 @@
-﻿using Bot.Tools;
+﻿using AdaptiveCards;
+using Bot.Tools;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +9,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.UI.WebControls;
 
 namespace Bot
 {
@@ -33,70 +36,190 @@ namespace Bot
             if (activity.Type == ActivityTypes.Message)
             {
                 var connectorClient = new ConnectorClient(new Uri(activity.ServiceUrl));
-                var tokens = activity.Text.ToTokens();
                 var reply = activity.CreateReply();
-                switch (tokens[0].ToLower())
+                if (string.IsNullOrEmpty(activity.Text) && activity.Value != null)
                 {
-                    case AddCommandName:
-                        if (tokens.Length < 2)
-                        {
-                            reply.Text = "Using: add \"Books name\"";
-                        }
-                        else
-                        {
-                            _app.Do(CommandHelper.CreateAdd(tokens[1]));
-                            reply.Text = "Book was added";
-                        }
-                        break;
+                    var reviewData = (JObject)activity.Value;
+                    var bookId = reviewData.GetValue("guid").Value<string>();
+                    var bookGuid = Guid.Parse(bookId);
+                    var rate = reviewData.GetValue("rate").Value<string>();
+                    var rateAsInt = int.Parse(rate);
+                    var reviewText = reviewData.GetValue("review").Value<string>() ?? string.Empty;
+                    var reader = reviewData.GetValue("reader").Value<string>() ?? string.Empty;
+                    var tagsString = reviewData.GetValue("tags").Value<string>() ?? string.Empty;
+                    var tags = tagsString.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                    _app.Do(CommandHelper.CreateReview(bookGuid, rateAsInt, reviewText, reader, tags));
+                    reply.Text = "Review added!";
+                }
+                else
+                {
+                    var tokens = activity.Text.ToTokens();
+                    switch (tokens[0].ToLower())
+                    {
+                        case AddCommandName:
+                            if (tokens.Length < 2)
+                            {
+                                reply.Text = "Using: add \"Books name\"";
+                            }
+                            else
+                            {
+                                _app.Do(CommandHelper.CreateAdd(tokens[1]));
+                                reply.Text = "Book was added";
+                            }
+                            break;
 
-                    case BooksCommandName:
-                        var someList = new List<Tuple<string, Guid>>
-                        {
-                            new Tuple<string, Guid> ("книга 1", Guid.NewGuid() ),
-                            new Tuple<string, Guid> ("книга 2", Guid.NewGuid() ),
-                            new Tuple<string, Guid> ("книга 3", Guid.NewGuid() ),
-                        };
-                        var card = new HeroCard
-                        {
-                            Title = "Books list",
-                        };
-                        var books = someList.Select(x => new CardAction
-                        {
-                            Type = PostBackName,
-                            Title = x.Item1,
-                            Value = ShowBookDetailsCommandName+" "+ x.Item2.ToString("N")
-                        }).ToList();
-                        card.Buttons = books;
-                        reply.Attachments.Add(card.ToAttachment());
-                        break;
-                    case ShowBookDetailsCommandName:
-                        var bookId = Guid.Parse(tokens[1]);
-                        var bookDetails = "НАЗЫАГие";
-                        var book = new HeroCard
-                        {
-                            Title = "Book details:",
-                            Subtitle = bookDetails,
-                            Text = "Book id: "+bookId.ToString("N")
-                        };
-                        var actions = new List<CardAction>
+                        case BooksCommandName:
+                            var someList = _app.Books(new string[0]);
+                            var card = new HeroCard
+                            {
+                                Title = "Books list",
+                            };
+                            var books = someList.Select(x => new CardAction
+                            {
+                                Type = PostBackName,
+                                Title = x.Name,
+                                Value = ShowBookDetailsCommandName + " " + x.Id.ToString("N")
+                            }).ToList();
+                            card.Buttons = books;
+                            reply.Attachments.Add(card.ToAttachment());
+                            break;
+
+                        case ShowBookDetailsCommandName:
+                            var bookId = Guid.Parse(tokens[1]);
+                            var bookInfo = _app.Books(new string[0]).FirstOrDefault(x => x.Id == bookId);
+                            if (bookInfo == null)
+                            {
+                                reply.Text = "Can't get book details";
+                                break;
+                            }
+                            var bookDetails = bookInfo.Name;
+                            var book = new HeroCard
+                            {
+                                Title = "Book details:",
+                                Subtitle = bookDetails,
+                                Text = "Book id: " + bookId.ToString("N")
+                            };
+                            var actions = new List<CardAction>
                         {
                             new CardAction
                             {
                                 Title = "Read",
                                 Type = PostBackName,
-                                Value = "start "+bookId.ToString("N")
+                                Value = "start " + bookId.ToString("N")
                             },
                             new CardAction
                             {
                                 Title = "Finish",
                                 Type = PostBackName,
-                                Value = "finish "+bookId.ToString("N")
+                                Value = "finish " + bookId.ToString("N")
                             },
+                            new CardAction
+                            {
+                                Title = "Review",
+                                Type = PostBackName,
+                                Value = "review " + bookId.ToString("N")
+                            }
                         };
 
-                        book.Buttons = actions;
-                        reply.Attachments.Add(book.ToAttachment());
-                        break;
+                            book.Buttons = actions;
+                            reply.Attachments.Add(book.ToAttachment());
+                            break;
+
+                        case "review":
+                            var id = Guid.Parse(tokens[1]);
+                            var bookInf = _app.Books(new string[0]).FirstOrDefault(b => b.Id == id);
+                            if (bookInf == null)
+                            {
+                                reply.Text = "Can't get book details";
+                                break;
+                            }
+                            var ca = new AdaptiveCard
+                            {
+                                Title = "Review Book:",
+                                Body = new List<CardElement>
+                            {
+                                new TextBlock
+                                {
+                                    Text = "Book:",
+                                    Size = TextSize.ExtraLarge
+                                },
+
+                                new TextBlock
+                                {
+                                    Text = bookInf.Name,
+                                    Size = TextSize.Large
+                                },
+                                new TextBlock
+                                {
+                                    Text = "Your name:",
+                                },
+                                new TextInput
+                                {
+                                    Id = "reader",
+                                },
+                                new TextBlock
+                                {
+                                    Text = "Rate book:"
+                                },
+                                new ChoiceSet
+                                {
+                                    Choices = new List<Choice>
+                                    {
+                                        new Choice {Title = "5 (Good)", Value = "5", IsSelected = true},
+                                        new Choice {Title = "4", Value = "4"},
+                                        new Choice {Title = "3", Value = "3"},
+                                        new Choice {Title = "2", Value = "2"},
+                                        new Choice {Title = "1 (Bad)", Value = "1"},
+                                    },
+                                    Style = ChoiceInputStyle.Expanded,
+                                    Id = "rate",
+                                    IsRequired = true
+                                },
+                                new TextBlock
+                                {
+                                    Text = "Review:"
+                                },
+                                new Input
+                                {
+                                    Id = "review",
+                                    Type = "Input.Text",
+                                },
+
+                                new TextBlock
+                                {
+                                    Text = "Tags (comma-separated):"
+                                },
+                                new TextInput
+                                {
+                                    Id = "tags"
+                                },
+                                new TextBlock
+                                {
+                                    Text = "Book id:",
+                                },
+                                new TextInput
+                                {
+                                    Id = "guid",
+                                    Value = bookInf.Id.ToString("N"),
+                                }
+                            },
+                                Actions = new List<ActionBase>
+                            {
+                                new SubmitAction
+                                {
+                                    Title = "Submit",
+                                    Type = "Action.Submit",
+                                }
+                            }
+                            };
+                            reply.Attachments.Add(new Attachment
+                            {
+                                ContentType = AdaptiveCard.ContentType,
+                                Content = ca
+                            });
+
+                            break;
+                    }
                 }
                 await connectorClient.Conversations.ReplyToActivityAsync(reply);
             }
--- Bot/Tools/CommandHelper.cs
+++ Bot/Tools/CommandHelper.cs
@@ -13,5 +13,17 @@ namespace Bot.Tools
                 Name = name
             };
         }
+
+        public static ICommand CreateReview(Guid id, int rate, string review, string readerName, string[] tags)
+        {
+            return new Review
+            {
+                Id = id,
+                Comment = review,
+                Rating = rate,
+                Reader = readerName,
+                Tags = tags
+            };
+        }
     }
 }
